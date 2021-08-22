d481203c9d648d44f19754d97a9c0a9d95ee93df
Adeolu <apata.adeoluwa@gmail.com>
2021-08-22 13:29:32 +0100

    calender and map usage

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div
+      className="flex justify-around items-center px-8 pb-3 w-full mx-auto sm:px-16
+    bg-gray-100 text-gray-600 py-16"
+    >
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How we work</p>
+        <p>Newsroom</p>
+        <p>Investor</p>
+        <p>Plus</p>
+        <p>Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Home</p>
+        <p>Accessibility</p>
+        <p>Awesome Design</p>
+        <p>Referrals accepted</p>
+        <p>Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">MOST</h5>
+        <p>React</p>
+        <p>Presents</p>
+        <p>Hero House</p>
+        <p>Hundreds of Students</p>
+        <p>Join Now</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYWRlb2x1d2EtOTYiLCJhIjoiY2tzbDVoa3pzMnVhaDJxb2Q3MTZ0anNqNSJ9.x9xSStDWY95zhDNO1gX3YQ",
+  },
+};
